c3dba62690cd855c427311600eddb4bb090c1013
Brian P O'Rourke <bpo@somnambulance.net>
2014-06-24 10:48:12 -0700

    Index calc is more intuitive after the sort.

--- rquantile.lua
+++ rquantile.lua
@@ -2,8 +2,9 @@
 
 local res = KEYS[1]
 local quantile = tonumber(ARGV[1])
-local index = math.ceil( quantile * redis.call('LLEN', res) ) - 1
 
 redis.call('SORT', res, 'STORE', res)
+
+local index = math.ceil( quantile * redis.call('LLEN', res) ) - 1
 
 return redis.call('LINDEX', res, index)
